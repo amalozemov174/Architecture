7698b67ae2e4f7e22f0c9047c9693893b90576ba
Anatoly <glenglen74@gmail.com>
2022-10-29 08:16:58 +0500

    Task1

---
+++ ModelElements/src/main/java/org/geekbrains/Camera.java
@@ -0,0 +1,16 @@
+package org.geekbrains;
+
+import java.awt.geom.Point2D;
+
+public class Camera {
+    Point2D location;
+    Angle3DS angle;
+
+    public void rotate(Angle3D angle) {
+
+    }
+
+    public void move(Point2D point) {
+
+    }
+}
---
+++ ModelElements/src/main/java/org/geekbrains/Poligon.java
@@ -0,0 +1,6 @@
+package org.geekbrains;
+import java.awt.geom.Point2D;
+
+public class Poligon {
+    Point2D points;
+}
